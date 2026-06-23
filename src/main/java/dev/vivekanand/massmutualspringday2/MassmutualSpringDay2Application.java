7882823832f8a49c304a87a59bdd1ec4367e3f64
Vivekanand Pujar <vivek@vivekanand.dev>
2026-06-23 14:42:41 +0530

    Initial commit

---
+++ src/main/java/dev/vivekanand/massmutualspringday2/MassmutualSpringDay2Application.java
@@ -0,0 +1,13 @@
+package dev.vivekanand.massmutualspringday2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MassmutualSpringDay2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MassmutualSpringDay2Application.class, args);
+	}
+
+}
---
+++ src/test/java/dev/vivekanand/massmutualspringday2/MassmutualSpringDay2ApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.vivekanand.massmutualspringday2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MassmutualSpringDay2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
