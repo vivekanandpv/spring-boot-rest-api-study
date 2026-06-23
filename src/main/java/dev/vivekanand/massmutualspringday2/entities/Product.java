d68db9bfc894c1dc0321a721038fce8c471fc012
Vivekanand Pujar <vivek@vivekanand.dev>
2026-06-23 15:12:37 +0530

    done

---
+++ src/main/java/dev/vivekanand/massmutualspringday2/entities/Product.java
@@ -0,0 +1,57 @@
+package dev.vivekanand.massmutualspringday2.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String description;
+    private String sku;
+    private String storeInformation;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getSku() {
+        return sku;
+    }
+
+    public void setSku(String sku) {
+        this.sku = sku;
+    }
+
+    public String getStoreInformation() {
+        return storeInformation;
+    }
+
+    public void setStoreInformation(String storeInformation) {
+        this.storeInformation = storeInformation;
+    }
+}
