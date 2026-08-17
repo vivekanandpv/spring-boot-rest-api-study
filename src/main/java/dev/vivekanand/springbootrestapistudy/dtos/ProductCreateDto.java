2c1b4a07a033db984a1939935e459a5f79e8f964
Vivekanand Pujar <vivek@vivekanand.dev>
2026-08-17 22:02:45 +0530

    Rename project to spring-boot-rest-api-study

--- src/main/java/dev/vivekanand/springbootrestapistudy/dtos/ProductCreateDto.java
+++ src/main/java/dev/vivekanand/springbootrestapistudy/dtos/ProductCreateDto.java
@@ -1,27 +1,26 @@
-package dev.vivekanand.massmutualspringday2.entities;
+package dev.vivekanand.springbootrestapistudy.dtos;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 
-@Entity
-public class Product {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+public class ProductCreateDto {
+    @NotBlank
+    @Size(min = 3, max = 100)
     private String name;
+
+    @NotBlank
+    @Size(max = 200)
     private String description;
+
+    @NotBlank
+    @Pattern(regexp = "^[A-Z]{5}[0-9]{5}$")
     private String sku;
+
+    @NotBlank
+    @Size(min = 10, max = 250)
     private String storeInformation;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
 
     public String getName() {
         return name;
