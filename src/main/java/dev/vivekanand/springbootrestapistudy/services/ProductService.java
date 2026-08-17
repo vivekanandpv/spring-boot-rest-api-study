3ea9f468d7d2320552ebf730d3d6b286b9994f5e
Vivekanand Pujar <vivek@vivekanand.dev>
2026-08-17 22:02:45 +0530

    Rename project to spring-boot-rest-api-study

--- src/main/java/dev/vivekanand/springbootrestapistudy/services/ProductService.java
+++ src/main/java/dev/vivekanand/springbootrestapistudy/services/ProductService.java
@@ -1,8 +1,8 @@
-package dev.vivekanand.massmutualspringday2.services;
+package dev.vivekanand.springbootrestapistudy.services;
 
-import dev.vivekanand.massmutualspringday2.dtos.ProductCreateDto;
-import dev.vivekanand.massmutualspringday2.dtos.ProductDto;
-import dev.vivekanand.massmutualspringday2.dtos.ProductUpdateDto;
+import dev.vivekanand.springbootrestapistudy.dtos.ProductCreateDto;
+import dev.vivekanand.springbootrestapistudy.dtos.ProductDto;
+import dev.vivekanand.springbootrestapistudy.dtos.ProductUpdateDto;
 
 import java.util.List;
 
